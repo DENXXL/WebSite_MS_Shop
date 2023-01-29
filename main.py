5785971534509b39db0428442b0ec4396145964a
Den <martynovich_deniska@mail.ru>
2023-01-29 14:30:07 +0300

    update project

--- main.py
+++ main.py
@@ -1,25 +1,64 @@
-from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask, render_template, request, redirect
+from cloudipsp import Api, Checkout
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(50), nullable = False)
-    price = db.Column(db.Integer, nullable = False)
-    IsActive = db.Column(db.Boolean, default = True)
+    title = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    IsActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return self.title
 
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    items = Item.query.order_by(Item.price).all()
+    return render_template('home.html', data=items)
 
-@app.route("/about_us")
+
+@app.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template('about.html')
 
+
+@app.route('/buy/<int:id>')
+def item_buy(id):
+    item = Item.query.get(id)
+    api = Api(merchant_id=1396424,
+              secret_key='test')
+    checkout = Checkout(api=api)
+    data = {
+        "currency": "RUB",
+        "amount": str(item.price) + "00"
+    }
+    url = checkout.url(data).get('checkout_url')
+    return redirect(url)
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == "POST":
+        title = request.form['title']
+        price = request.form['price']
+
+        item = Item(title=title, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Ошибочка вышла :("
+    else:
+        return render_template('create.html')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
+
